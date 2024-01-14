1b4a6947e1fde4de0638cad6e5679d474dc275d7
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2024-01-14 18:57:08 +0530

    first commit

---
+++ src/_root/pages/Saved.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Saved = () => {
+  return (
+    <div className='mt-10 w-full text-center'>
+        <h2>This Section Is Under Development</h2>
+    </div>
+  )
+}
+
+export default Saved
